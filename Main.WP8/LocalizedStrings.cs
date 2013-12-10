cf990bb89635f80ff365a063dae67e2bdbef7037
ig.ivanov <ig.ivanov@qiwi.ru>
2013-12-10 11:58:49 +0400

    Initial commit

---
+++ Main.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Main.WP8.WP8Resources;
+
+namespace Main.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
